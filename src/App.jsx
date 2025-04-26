e636df79ca0b1191235483dbca889df61588cc62
Love Poonia <chlovepoonia26@gmail.com>
2025-04-26 13:57:12 +0530

    navbar making using tailwind + daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 
-import './App.css'
-
 function App() {
 
 
   return (
     <>
-    <div className="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+   <div className="navbar bg-base-300">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">👩‍💻DevTinder</a>
+  </div>
+  <div className="flex-none">
+    <div className="dropdown dropdown-end mx-5">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
     </div>
   </div>
 </div>
