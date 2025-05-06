b607f2d0ecd1f00c9b82a42489411612370cb740
Love Poonia <chlovepoonia26@gmail.com>
2025-05-06 11:15:26 +0530

    update baseurl

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,9 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
+   if(location.hostname === "localhost"){
     return io(BASE_URL);
+   } else {
+    return io("/" , {path:"https://devtinder-backend-zes8.onrender.com/socket.io"})
+   }
 }
